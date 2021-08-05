1f6b1b5f40f764f13dc66fcc25d6eca93725baf3
ersaumya <ersaumyarout@gmail.com>
2021-08-05 15:46:13 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scroll-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'InfiniteScrollApp';
+}
